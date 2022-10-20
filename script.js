e4d01582723dbac6a375c8567361b705c9ce64d7
Patrik Fiala <89139103+Bl3sk@users.noreply.github.com>
2022-10-20 17:54:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,116 @@
 class Calculator{
-  constructor(displayRes){
-    this.displayRes = displayRes
+  constructor(){
     this.currentOperand = 0
     this.previousOperand = 0
-    this.clear()
+    this.operation = null
+    this.overwriteCurrent = false
+    this.defaultStyleOp = false
   }
   clear(){
+    //console.log("clear")
     this.currentOperand = 0
     this.previousOperand = 0
     this.operation = null
   }
   del(){
-    this.currentOperand = 0
-    this.previousOperand = 0
-    displayRes.innerHTML = this.previousOperand
-    btnDelete.innerHTML = "AC"
+    if(btnDelete.innerHTML === "C"){
+      this.currentOperand = 0
+      btnDelete.innerHTML = "AC"
+    }else if(btnDelete.innerHTML === "AC"){
+      this.clear()
+    }
   }
   addDigit(digit){
-    if(this.currentOperand == 0){
+    this.defaultStyleOp = true
+    if(this.currentOperand == 0 || this.overwriteCurrent){
       this.currentOperand = digit
       btnDelete.innerHTML = "C"
+      this.overwriteCurrent = false
     } 
     else if(this.currentOperand != 0){
+      this.overwriteCurrent = false
       this.currentOperand = this.currentOperand + digit
     } 
-    console.log(this.currentOperand)
+    //console.log(this.currentOperand)
   }
-  getOperator(operator, buttonClass){
-    let buttonOperator = document.querySelector(`.${buttonClass}`)
-    buttonOperator.style.backgroundColor = "white";
-    this.operator = operator
+  sendOperator(operator){
+    if(operator === "="){
+      this.defaultStyleOp = true
+      switch (this.operation) {
+        case "÷":
+          this.currentOperand = Number(this.previousOperand) / Number(this.currentOperand)
+          break;
+        case "×":
+          this.currentOperand = Number(this.previousOperand) * Number(this.currentOperand)
+          break;
+        case "-":
+          this.currentOperand = Number(this.previousOperand) - Number(this.currentOperand)
+          break;
+        case "+":
+          this.currentOperand = Number(this.previousOperand) + Number(this.currentOperand)
+          break;
+      }
+      return
+    }
+    this.defaultStyleOp = false
+    this.overwriteCurrent = true
+    this.operation = operator
     this.previousOperand = this.currentOperand
-    this.currentOperand = 0
   }
-  updateDis(){
-    console.log("updateDis")
+  updateOperator(){
+    let buttons = document.querySelectorAll(`.btn-operator`)
+    buttons.forEach(button => {
+      //console.log(this.operation)
+      if(button.textContent == this.operation && !this.defaultStyleOp){
+        button.style.backgroundColor = "white";
+        button.style.color = "orange";
+      }else if(button.textContent != this.operation || this.defaultStyleOp){
+        button.style.backgroundColor = "orange";
+        button.style.color = "white";
+      }
+    });
+  }
+  updateCalc(){
+    this.updateOperator()
     displayRes.innerHTML = this.currentOperand
   }
 }
 
+const hourElem = document.querySelector(".hour")
+const minuteElem = document.querySelector(".minute")
+setInterval(()=>{
+  let date = new Date()
+  let hour = date.getHours()
+  let minute = date.getMinutes()
+  if(String(minute).length < 2){
+    hourElem.innerHTML = hour
+    minuteElem.innerHTML = "0" + minute
+  }else if(String(minute).length > 1){
+    hourElem.innerHTML = hour
+    minuteElem.innerHTML = minute
+  }
+}, 1000)
 
-const btns = document.querySelector(".grid-container")
-const displayRes = document.querySelector("#result")
+const btnsDigit = document.querySelectorAll(".btn-digit")
+const btnsOperator = document.querySelectorAll(".btn-operator")
 const btnDelete = document.querySelector(".btn-delete")
+const displayRes = document.querySelector("#result")
 
-const calc = new Calculator(displayRes)
+const calc = new Calculator()
 
-btns.addEventListener('click', (e)=>{
-  switch (e.target.className) {
-  case "btn-digit":
+btnsDigit.forEach(button => {
+  button.addEventListener('click', (e)=>{
     calc.addDigit(e.target.textContent)
-    calc.updateDis()
-    break;
-  case "btn-operator":
-    console.log(e.target)
-    this.style.backgroundColor = "red";
-    calc.getOperator(e.target.textContent, e.target.className)
-    break;
-  case 2:
-     day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case "btn-delete":
-    calc.del()
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-}
+    calc.updateCalc()
+  });
+})
+btnsOperator.forEach(operator => {
+  operator.addEventListener('click', (e)=>{
+    calc.sendOperator(e.target.textContent)
+    calc.updateCalc()
+  });
+})
+btnDelete.addEventListener('click', (e)=>{
+  calc.del()
+  calc.updateCalc()
 });
-
-
-
-
